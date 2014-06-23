5430b5ee2b20d4e1e868628d176d101660a1311c
Artem Polikarpov <artpolikarpov@gmail.com>
2014-06-23 14:28:37 +0400

    Allow multiple assets: `asset: asset1` → `asset: [asset1, asset2]`

--- tasks/gh_release.js
+++ tasks/gh_release.js
@@ -8,7 +8,8 @@
 
 'use strict';
 
-var curl = require('curlrequest');
+var curl = require('curlrequest'),
+    _ = require('underscore');
 
 module.exports = function(grunt) {
 
@@ -51,40 +52,48 @@ module.exports = function(grunt) {
           grunt.log.ok('Release “' + reply.name + '” has been created.');
 
           if (data.asset) {
-            if (!data.asset.file) done(new Error('Asset error: no file provided.'));
-            if (!data.asset['Content-Type']) done(new Error('Asset error: no content type of the asset provided.'));
-            if (!data.asset.name) {
-              data.asset.name = data.asset.file.replace(/(.*)\/(.*)$/, '$2');
-            }
+            var assets = _.isArray(data.asset) ? data.asset : [data.asset],
+                assetsSize = assets.length,
+                assetsDone = 0;
 
-            curl.request({
-              // Upload asset
-              url: 'https://uploads.github.com/repos/' + options.owner + '/' + options.repo + '/releases/' + reply.id + '/assets?name=' + data.asset.name,
-              method: 'POST',
-              headers: {
-                Accept: 'application/vnd.github.manifold-preview',
-                'Content-Type': data.asset['Content-Type']
-              },
-              user: options.token + ':x-oauth-basic',
-              'data-binary': '@' + data.asset.file
-            }, function (err, stdout) {
-              if (err) {
-                done(new Error(err));
-              } else {
-                var reply;
-
-                try {
-                  reply = JSON.parse(stdout);
-                } catch (e) {};
-
-                if (!reply || reply.errors || !reply.name) {
-                  done(new Error(reply ? reply.message + '\n' + JSON.stringify(reply.errors, null, '  ') : 'Problems parsing response.'));
-                } else {
-                  grunt.log.ok('Release asset “' + reply.name + '” has been ' + reply.state + '.');
-                }
+            _.each(assets, function (asset) {
+              if (!asset.file) done(new Error('Asset error: no file provided.'));
+              if (!asset['Content-Type']) done(new Error('Asset error: no content type of the asset provided.'));
+              if (!asset.name) {
+                asset.name = asset.file.replace(/(.*)\/(.*)$/, '$2');
               }
 
-              done();
+              curl.request({
+                // Upload asset
+                url: 'https://uploads.github.com/repos/' + options.owner + '/' + options.repo + '/releases/' + reply.id + '/assets?name=' + asset.name,
+                method: 'POST',
+                headers: {
+                  Accept: 'application/vnd.github.manifold-preview',
+                  'Content-Type': asset['Content-Type']
+                },
+                user: options.token + ':x-oauth-basic',
+                'data-binary': '@' + asset.file
+              }, function (err, stdout) {
+                if (err) {
+                  done(new Error(err));
+                } else {
+                  var reply;
+
+                  try {
+                    reply = JSON.parse(stdout);
+                  } catch (e) {}
+
+                  if (!reply || reply.errors || !reply.name) {
+                    done(new Error(reply ? reply.message + '\n' + JSON.stringify(reply.errors, null, '  ') : 'Problems parsing response.'));
+                  } else {
+                    grunt.log.ok('Release asset “' + reply.name + '” has been ' + reply.state + '.');
+                  }
+                }
+
+                assetsDone++;
+
+                assetsDone === assetsSize && done();
+              });
             });
           } else {
             done();
